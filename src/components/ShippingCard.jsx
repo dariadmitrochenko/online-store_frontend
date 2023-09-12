9e59bea5624a791a5d5f7793b194b346e2f65f39
Daria Dmitrochenko <dariadmitrochenko@Evgeny.lan>
2023-09-11 20:12:13 -0400

    Checkout layout fixed

--- src/components/ShippingCard.jsx
+++ src/components/ShippingCard.jsx
@@ -3,85 +3,68 @@ import React from "react";
 const ShippingCard = () => {
   return (
     <div>
-    <div className="py-16 px-4 md:px-6 2xl:px-0 flex justify-center items-center 2xl:mx-auto 2xl:container">
-      <div className="flex flex-col-1 justify-start items-start space-y-9">
-        <div className="p-8 bg-gray-100 dark:bg-gray-800 flex flex-col">
-          <label className="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">
-            Address Line 1
-          </label>
-          <div className="mt-2 flex-col">
-            <div>
-              <input
-                className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                type="address"
-                name=""
-                id=""
-                placeholder="Address Line 1"
-              />
+      <div className="py-16 px-0 md:px-6 2xl:px-0 justify-center items-center 2xl:mx-auto 2xl:container">
+        <div>
+          <div className="flex flex-col p-8 bg-gray-100 dark:bg-gray-800 xl:w-3/5">
+            <div className="mt-0.3">
+              <div>
+                <input
+                  className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                  type="address"
+                  name=""
+                  id=""
+                  placeholder="Address Line 1"
+                />
+              </div>
             </div>
-          </div>
-          <label className="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">
-            Address Line 2
-          </label>
-          <div className="mt-2 flex-col">
-            <div>
-              <input
-                className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                type="address"
-                name=""
-                id=""
-                placeholder="Address Line 2"
-              />
+            <div className="mt-4">
+              <div>
+                <input
+                  className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600"
+                  type="address"
+                  name=""
+                  id=""
+                  placeholder="Address Line 2"
+                />
+              </div>
             </div>
-          </div>
-          <label className="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">
-            City
-          </label>
-          <div className="mt-2 flex-col">
-            <div>
-              <input
-                className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                type="address"
-                name=""
-                id=""
-                placeholder="City"
-              />
+            <div className="mt-4">
+              <div>
+                <input
+                  className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600"
+                  type="address"
+                  name=""
+                  id=""
+                  placeholder="City"
+                />
+              </div>
             </div>
-          </div>
-          <label className="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">
-            State
-          </label>
-          <div className="mt-2 flex-col">
-            <div>
-              <input
-                className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                type="address"
-                name=""
-                id=""
-                placeholder="State"
-              />
+            <div className="mt-4">
+              <div>
+                <input
+                  className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600"
+                  type="address"
+                  name=""
+                  id=""
+                  placeholder="State (NY only)"
+                />
+              </div>
             </div>
-          </div>
-          <label className="mt-8 text-base leading-4 text-gray-800 dark:text-gray-50">
-            Zipcode
-          </label>
-          <div className="mt-2 flex-col">
-            <div>
-              <input
-                className="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
-                type="address"
-                name=""
-                id=""
-                placeholder="Zipcode"
-              />
+            <div className="mt-4">
+              <div>
+                <input
+                  className="border border-gray-300 p-4 rounded w-full text-base leading-4 placeholder-gray-600"
+                  type="number"
+                  name=""
+                  id=""
+                  placeholder="Zipcode"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    </div>
-    
   );
 };
 
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,8 +5,8 @@ import ShippingCard from "../components/ShippingCard";
 
 const Checkout = () => {
   return (
-    <div className="container mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-[30px] justify-between">
+    <div className="container mx-auto py-20">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 justify-between ">
         <ShippingCard />
         <PaymentCard />
       </div>
